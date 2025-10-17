01c641aec22df0de62bd3f94c1b5c2536e9a21f8
Oleg Ianchenko <olegian@cs.washington.edu>
2025-10-16 18:46:00 -0700

    uf impl and simple analysis

---
+++ src/main.rs
@@ -0,0 +1,129 @@
+mod union_find;
+use union_find::UnionFind;
+use std::{collections::HashMap, sync::{LazyLock, Mutex}};
+
+struct Site {
+    type_uf: UnionFind<String>,
+    var_tags: HashMap<String, String>,
+    observed_var_tags: Vec<(String, String)>,
+}
+
+impl Site {
+    pub fn new() -> Self {
+        Site {
+            type_uf: UnionFind::new(),
+            var_tags: HashMap::new(),
+            observed_var_tags: Vec::new(),
+        }
+    }
+
+    pub fn observe_var(&mut self, var: String, tag: String) {
+        self.observed_var_tags.push((var, tag));
+    }
+
+    pub fn update(&mut self, value_uf: &mut UnionFind<String>) {
+        for (new_var, new_var_tag) in &self.observed_var_tags {
+            let new_leader_tag = value_uf.find(new_var_tag).unwrap(); // ? is this unwrap safe? 
+            self.type_uf.make_set(new_leader_tag.clone());
+
+            if let Some(old_tag) = self.var_tags.get(new_var) {
+                let old_leader_tag = value_uf.find(old_tag).unwrap();
+
+                let merged = self.type_uf.union(&old_leader_tag, &new_leader_tag).unwrap();
+                self.var_tags.insert(new_var.clone(), merged);
+            } else {
+                self.var_tags.insert(new_var.clone(), new_leader_tag);
+            }
+        }
+    }
+
+    pub fn get_leaders(&self) -> &HashMap<String, String> {
+        &self.var_tags
+    }
+}
+
+static VALUE_UF: LazyLock<Mutex<UnionFind<String>>> = LazyLock::new(|| Mutex::new(UnionFind::new()));
+static SITE_UFS: LazyLock<Mutex<Sites>> = LazyLock::new(|| Mutex::new(Sites::new()));
+
+struct Sites {
+    locs: HashMap<usize, Site>,
+}
+impl Sites {
+    pub fn new() -> Self {
+        Sites { locs: HashMap::new()}
+    }
+
+    pub fn get_site(&mut self, id: usize) -> &mut Site {
+        if !self.locs.contains_key(&id) {
+            self.locs.insert(id, Site::new());
+        }
+
+        self.locs.get_mut(&id).unwrap()
+    }
+
+    pub fn print_analysis(&self) {
+        for (id, site) in self.locs.iter() {
+            println!("=== AT SITE {} ===", id);
+            for (var, leader) in site.get_leaders() {
+                println!("{var} -> {leader}");
+            }
+        }
+    }
+}
+
+fn main() {
+    simple_func(10, 100);
+    simple_func(20, 200);
+    simple_func(30, 300);
+
+    let site_ufs = SITE_UFS.lock().unwrap();
+    site_ufs.print_analysis();
+}
+
+fn simple_func(x: u32, y: u32) -> u32 {
+    let mut value_uf = VALUE_UF.lock().unwrap();
+    let mut site_ufs = SITE_UFS.lock().unwrap();
+    let site = site_ufs.get_site(0);
+
+    value_uf.make_set(format!("VAL:{}", x));  // for parameter input
+    value_uf.make_set(format!("VAL:{}", y));
+    site.observe_var("VAR:x".into(), format!("VAL:{}", x));
+    site.observe_var("VAR:y".into(), format!("VAL:{}", y));
+
+    let a: u32 = 2;
+    value_uf.make_set(format!("LIT:{}", 2));
+    value_uf.make_set(format!("VAL:{}", a));
+    site.observe_var("VAR:a".into(), format!("VAL:{}", a));
+
+    let b: u32 = 3;
+    value_uf.make_set(format!("LIT:{}", 3));
+    value_uf.make_set(format!("VAL:{}", b));
+    site.observe_var("VAR:b".into(), format!("VAL:{}", b));
+
+    let result = a + x;
+    value_uf.make_set(format!("VAL:{}", result));
+    value_uf.union(&format!("VAL:{}", result), &format!("VAL:{}", a));
+    value_uf.union(&format!("VAL:{}", result), &format!("VAL:{}", x));
+    site.observe_var("VAR:result".into(), format!("VAL:{}", result));
+
+    let test = b + y;
+    value_uf.make_set(format!("VAL:{}", test));
+    value_uf.union(&format!("VAL:{}", test), &format!("VAL:{}", b));
+    value_uf.union(&format!("VAL:{}", test), &format!("VAL:{}", y));
+    site.observe_var("VAR:test".into(), format!("VAL:{}", test));
+
+    if test > 300 {
+        // TODO: THIS IMPLEMENTAITON REQUIRES SSA FORM!
+        // is that fine ???
+        // basically because value_uf has to incorporate both old result here and result2 with tags for proper merging
+        let result2 = result + test;
+        value_uf.make_set(format!("VAL:{}", result2));
+        value_uf.union(&format!("VAL:{}", result2), &format!("VAL:{}", result));
+        value_uf.union(&format!("VAL:{}", result2), &format!("VAL:{}", test));
+        site.observe_var("VAR:result2".into(), format!("VAL:{}", result2));
+    }
+
+    site.update(&mut value_uf);
+
+    return result
+}
---
+++ src/union_find.rs
@@ -0,0 +1,94 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+pub struct UnionFind<Tag>
+where
+    Tag: Eq + Hash + Clone,
+{
+    tag_to_index: HashMap<Tag, usize>,
+    index_to_tag: Vec<Tag>,
+    parent: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl<Tag> UnionFind<Tag>
+where
+    Tag: Eq + Hash + Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            tag_to_index: HashMap::new(),
+            index_to_tag: Vec::new(),
+            parent: Vec::new(),
+            rank: Vec::new(),
+        }
+    }
+
+    /// Returns Some(idx) if this tag already corresponds to some set
+    /// at parent[i] with rank[i]. Returns None if this a new set
+    pub fn make_set(&mut self, tag: Tag) -> Option<usize> {
+        if self.tag_to_index.contains_key(&tag) {
+            return Some(*self.tag_to_index.get(&tag).unwrap());
+        }
+
+        let index = self.parent.len();
+        self.tag_to_index.insert(tag.clone(), index);
+        self.index_to_tag.push(tag);
+        self.parent.push(index);
+        self.rank.push(0);
+        
+        None
+    }
+
+    fn get_index(&self, tag: &Tag) -> Option<usize> {
+        self.tag_to_index.get(tag).copied()
+    }
+
+    /// Find the leader tag for the set containing `tag`.
+    pub fn find(&mut self, tag: &Tag) -> Option<Tag> {
+        let index = self.get_index(tag)?;
+        let leader_index = self.find_index(index);
+        Some(self.index_to_tag[leader_index].clone())
+    }
+
+    /// Returns the leader tag of the merged set.
+    pub fn union(&mut self, tag1: &Tag, tag2: &Tag) -> Option<Tag> {
+        let i1 = self.get_index(tag1)?;
+        let i2 = self.get_index(tag2)?;
+        let leader_index = self.union_indices(i1, i2);
+        Some(self.index_to_tag[leader_index].clone())
+    }
+
+    /// w/ path compression
+    fn find_index(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find_index(self.parent[x]);
+        }
+        self.parent[x]
+    }
+
+    /// union by rank
+    fn union_indices(&mut self, x: usize, y: usize) -> usize {
+        let x_root = self.find_index(x);
+        let y_root = self.find_index(y);
+
+        if x_root == y_root {
+            return x_root;
+        }
+
+        // Union by rank
+        if self.rank[x_root] < self.rank[y_root] {
+            self.parent[x_root] = y_root;
+            y_root
+        } else if self.rank[x_root] > self.rank[y_root] {
+            self.parent[y_root] = x_root;
+            x_root
+        } else {
+            self.parent[y_root] = x_root;
+            self.rank[x_root] += 1;
+            x_root
+        }
+    }
+
+
+}
